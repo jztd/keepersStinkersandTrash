c6465b6b5e37e7d3e3060f08ee3e9eba942317ea
Justin <potter1990@msn.com>
2021-10-08 20:40:39 -0700

    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import path from 'path'
+
+let app = express();
+const __dirname = path.resolve();
+
+app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Hello From Express' });
+});
+
+app.get('/recipes', (req, res) => {
+    res.send("SPAGHETI IS GOOD IF FRIED");
+});
+  
+app.post('/api/world', (req, res) => {
+    res.send(
+        `I received your POST request. This is what you sent me: ${req.body.post}`,
+    );
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/fst/build/index.html'));
+  });
+
+const port = process.env.PORT || 5000;
+app.listen(port);
